87370373fe824f89f7663869af449afec80b559f
ImamRivaldi <32257746+ImamRivaldi@users.noreply.github.com>
2017-11-08 22:31:19 +0800

    Perbaikan
    Proses jika jam kerja di atas 40 atau kurang.

--- soal3.cpp
+++ soal3.cpp
@@ -4,14 +4,20 @@
 using namespace std;
 
 int main(){
-	int gajipokok,jamkerja,gajitotal,pajak,lembur;
+	int gajipokok,jamkerja,gajitotal,pajak,lembur,totalgaji;
 	cout<<"Jumlah Gaji Pegawai		: ";
 	cin>>gajipokok;
 	cout<<"Jam Kerja			: ";
 	cin>>jamkerja;
-	lembur=jamkerja-40;
-	gajitotal=(lembur*1.5+40)+gajipokok;
+	if(jamkerja>40){
+	lembur=(jamkerja-40)*(gajipokok/40);
+	gajitotal=(lembur*1.5)+gajipokok;
 	pajak=gajitotal*0.15;
-	gajitotal=gajitotal-pajak;
-	cout<<"Total Gaji Bersih Pegawai	: "<<gajitotal<<endl;
+	totalgaji=gajitotal-pajak;
+	}
+	else{
+		pajak=gajipokok*0.15;
+		totalgaji=gajipokok-pajak;
+	}
+	cout<<"Total Gaji Bersih Pegawai	: "<<totalgaji<<endl;
 }
